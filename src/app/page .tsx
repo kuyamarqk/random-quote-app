465ba954f3a8c81e7f12812f3e721e22ac08f316
Raymart G. Maqueda <kuyamarqk@gmail.com>
2023-07-08 20:17:27 +0800

    final

---
+++ src/app/page .tsx
@@ -0,0 +1,41 @@
+"use client";
+import { useState } from "react";
+
+interface Quote {
+  content: string;
+  author: string;
+} 
+
+export default function Home() {
+  const [data, setData] = useState<Quote | null>(null);
+
+  const handleClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*', // Replace '*' with the actual domain that is making the request
+      },
+    };
+
+    try {
+      const response = await fetch('https://api.quotable.io/random', requestOptions);
+      const quoteData = await response.json();
+      console.log(quoteData);
+      setData(quoteData);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div className="text-center rounded-20 m-20 p-56 bg-yellow-300">
+      <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl mb-10">Food for Thoughts</h2>
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-10">{data?.content}</h1>
+      <p className="text-lg text-gray-500">{data?.author}</p>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClick}>Generate</button>
+    </div>
+  );
+}
